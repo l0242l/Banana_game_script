3a5b26f72fae67872bd91b2acf7b38903818b597
l0242l <charley.cheng1@gmail.com>
2024-05-08 12:06:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-#banana
+# banana
+"""
+if these imports are red install them by typing
+" pip install (the thing that is red)" 
+into the terminal 
+
+"""
+
 from AppOpener import open, close
 import time
 import math
-import 
-open("banana")
+from pynput.mouse import Button, Controller
+import tkinter as tk
+
+mouse = Controller()
+app = "banana"
+
+# opens banana game
+time.sleep(2)
+
+#open(app)
+print("hello world")
+def MouseShit():
+    pos = mouse.position
+
+    root = tk.Tk()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    goal_x = screen_width / 2
+    goal_y = screen_height / 2
+    x = 
+    y = 
+    print(screen_height,screen_width)
+    #mouse.move(x, y)
+
+MouseShit()
+#close(app)
